7e34df14155de18dcc3ae57940d7a2adc8979dc6
alessandroBSilva <alessandrobrugnera.s@gmail.com>
2020-08-29 16:16:42 -0300

    Verificação do CEP feita1.

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import requests
 
 def main():
-    # cep = input("Insira um CEP com 8 algarismos (somente números): ")
-    cep = "01001000"
+    cepRecebido=False
+    while not cepRecebido:
+        cep = input("Insira um CEP com 8 algarismos (somente números): ")
+        if (len(str(cep))!=8):
+            print("CEP inválido, insira novamente.")
+        else:
+            cepRecebido=True
+    # cep = "01001000"
     url = "https://viacep.com.br/ws/{}/json/".format(cep)
     res = requests.get(url)
 
-    if (res.status_code==requests.codes.ok):
-        json=res.json()
+    json=res.json()
+    if ("erro" in json):
+        print("CEP inexistente.")
+    elif (res.status_code==requests.codes.ok):
         print("O endereço do CEP {} é:".format(cep))
         print("{} - {}, {} - {}.".format(json["logradouro"], json["bairro"], json["localidade"], json["uf"]))
+    else:
+        print("Problema com a requisição, tente novamente.")
 
 if __name__ == '__main__':
     main()
